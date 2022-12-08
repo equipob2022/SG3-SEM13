4e913916babac05eb7281107bb86f8eb79a924fb
Kevin Ramos Rivas <80496624+KevinRamosRivas@users.noreply.github.com>
2022-12-08 15:31:53 -0500

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 
 import streamlit as st
 from multiapp import MultiApp
-from apps import home
+from apps import home,model1,model2,model3,model4 # import your app modules here
 
 app = MultiApp()
 
@@ -10,10 +10,10 @@ st.markdown("""
 """)
 # Add all your application here
 app.add_app("Home", home.app)
-# app.add_app("Modelo SVR", model1.app)
-# app.add_app("Modelo Logistic Regression", model2.app)
-# app.add_app("Modelo GRU", model3.app)
-# app.add_app("Modelo ARIMA", model4.app)
+app.add_app("Modelo SVR", model1.app)
+app.add_app("Modelo Logistic Regression", model2.app)
+app.add_app("Modelo GRU", model3.app)
+app.add_app("Modelo ARIMA", model4.app)
 
 # The main app
 app.run()
