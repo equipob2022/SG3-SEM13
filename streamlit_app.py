e765a2cbdcd39029b1f31c0e52febbe5d23d8373
pedro <araujosantillanp8@gmail.com>
2022-12-09 23:13:17 -0500

    avance LSTM

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from multiapp import MultiApp
 # import los modelos aqui
-from apps import model10, home,model12, model11
+from apps import model9,model10, home,model12, model11
 
 app = MultiApp()
 
@@ -13,6 +13,7 @@ st.markdown("""
 app.add_app("Home", home.app)
 # app.add_app("Modelo SVR", model1.app)
 # app.add_app("PCA and Hierarchical Portfolio Optimisation", model12.app)
+app.add_app("Modelo LSTM", model9.app)
 app.add_app("Modelo XGBoost", model10.app)
 app.add_app("Modelo Random Forest Regressor", model11.app)
 app.add_app("PCA and Hierarchical Portfolio Optimisation", model12.app)
