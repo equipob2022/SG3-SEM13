c12954ac3625a12859590cf164298993619a2135
Kevin Ramos Rivas <80496624+KevinRamosRivas@users.noreply.github.com>
2022-12-10 01:36:30 -0500

    se sube el  modelo LSTM corregido

--- apps/model9.py
+++ apps/model9.py
@@ -1,189 +1,186 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import pandas_datareader as datas
-from tensorflow.keras.models import load_model
 import streamlit as st
-from sklearn import metrics
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import pandas as pd
+import pandas_datareader as datas
 import plotly.express as px
-import tensorflow as tf
+from sklearn.preprocessing import MinMaxScaler
+import keras
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+import matplotlib.dates as mdates
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+import plotly.graph_objects as go
 
 
 def app():
-    st.title('Model - LSTM')
 
-    #start = '2004-08-18'
-    #end = '2022-01-20'
-    start = st.date_input('Start' , value=pd.to_datetime('2004-08-18'))
-    end = st.date_input('End' , value=pd.to_datetime('today'))
-
-    st.title('Predicción de tendencia de acciones')
-
-    user_input = st.text_input('Introducir cotización bursátil' , 'GOOG')
-
-    df = datas.DataReader(user_input, 'yahoo', start, end)
-
-    # Describiendo los datos
-
-    st.subheader('Datos del 2004 al 2022') 
-    st.write(df.describe())
-
-    #Visualizaciones 
-
-  
-    st.subheader('Closing Price vs Time chart')
-    fig = plt.figure(figsize = (12,6))
-    plt.plot(df.Close)
-    st.pyplot(fig)
-
+    start = st.date_input('Inicio(Start)',value=pd.to_datetime('2010-01-01'))
+    end = st.date_input('Fin' , value=pd.to_datetime('today'))
     
+    st.title('Predicción de tendencia de acciones usando LSTM')
 
-    st.subheader('Closing Price vs Time chart con 100MA')
-    ma100 = df.Close.rolling(100).mean()
-    fig = plt.figure(figsize = (12,6))
-    plt.plot(df.Close)
-    plt.plot(ma100,'r', label='ma100')
-    plt.legend()
-    st.pyplot(fig)
+    user_input = st.text_input('Introducir cotización bursátil' , 'DOGE-EUR')
+    dfi = datas.DataReader(user_input, 'yahoo', start, end)
+    #escribir un poco acerca de la empresa introducida en user_input
+    # con la libreria de pandas_datareader podemos obtener informacion de la empresa
+    st.subheader('Acerca de la empresa')
+    st.write(datas.get_quote_yahoo(user_input))
+    st.write(dfi)
 
-
-
-
-
-
-
-    st.subheader('Closing Price vs Time chart con 100MA & 200MA')
-    ma100 = df.Close.rolling(100).mean()
-    ma200 = df.Close.rolling(200).mean()
-    fig5 = plt.figure(figsize=(12,6))
-    plt.plot(df.Close)
-    plt.plot(ma100,'r', label='ma100')
-    plt.plot(ma200,'g', label='ma200')
-    plt.legend()
-    st.pyplot(fig5)
+    #separar el dataframe en train y test
+    df = dfi.iloc[:int(dfi.shape[0]*0.8), :]
+    test_df = dfi.iloc[int(dfi.shape[0]*0.8):, :]
 
 
 
+    # ordenar por fecha
+    df = df.sort_values('Date')
+    test_df = test_df.sort_values('Date')
 
-    # Splitting data into training and testing 
+    # fix the date 
+    df.reset_index(inplace=True)
+    df.set_index("Date", inplace=True)
+    test_df.reset_index(inplace=True)
+    test_df.set_index("Date", inplace=True)
 
-    # Cree un nuevo marco de datos con solo la columna 'Close'
-    data = df.filter(['Close'])
+    #hacemos un grafico de la serie de tiempo
+    st.subheader('Serie de tiempo de la cotización bursátil de '+user_input)
+    fig = px.line(dfi, x=dfi.index , y="Close", title='Precio de cierre de '+ user_input)
+    st.plotly_chart(fig)
 
-    # Convierte el marco de datos en una matriz numpy
-    dataset = data.values
+    # mostrar un grafico de la media movil de la serie de tiempo
+    st.subheader('Media movil de la cotización bursátil de '+user_input)
+    fig = px.line(dfi, x=dfi.index , y="Close", title='Precio de cierre de '+ user_input)
+    fig.add_scatter(x=dfi.index, y=dfi.Close.rolling(window=30).mean(), mode='lines', name='Media movil')
+    st.plotly_chart(fig)
 
-    # Obtenga el número de filas para entrenar el modelo
-    training_data_len = int(np.ceil( len(dataset) * .95 ))
 
-    # Escalando la data
+    import matplotlib.dates as mdates
+
+    # cambiar las fechas en enteros para el entrenamiento
+    dates_df = df.copy()
+    dates_df = dates_df.reset_index()
+
+    # Almacene las fechas originales para trazar las predicciones
+    org_dates = dates_df['Date']
+
+    # convert to ints
+    dates_df['Date'] = dates_df['Date'].map(mdates.date2num)
+
+    # Crear un conjunto de datos de entrenamiento de precios de 'Adj Close':
+    train_data = df.loc[:,'Adj Close'].to_numpy()
+
+
+    # Aplique la normalización antes de alimentar a LSTM usando sklearn:
     from sklearn.preprocessing import MinMaxScaler
 
-    scaler = MinMaxScaler(feature_range=(0,1))
-    scaled_data = scaler.fit_transform(dataset)
+    scaler = MinMaxScaler()
+    train_data = train_data.reshape(-1,1)
 
+    scaler.fit(train_data)
+    train_data = scaler.transform(train_data)
 
-    # Cargar mi modelo
+    '''Función para crear un conjunto de datos para alimentar un LSTM'''
+    def create_dataset(dataset, look_back):
+        dataX, dataY = [], []
+        for i in range(len(dataset)-look_back):
+            a = dataset[i:(i + look_back), 0]
+            dataX.append(a)
+            dataY.append(dataset[i + look_back, 0])
+        return np.array(dataX), np.array(dataY)
+        
+        
+    # Cree los datos para entrenar nuestro modelo en:
+    time_steps = 36
+    X_train, y_train = create_dataset(train_data, time_steps)
 
-    model = load_model('keras_model.h5')
-
-
-    # Create the testing data set
-    # Create a new array containing scaled values from index 1543 to 2002 
-    # Crear el conjunto de datos de prueba
-    # Crear una nueva matriz que contenga valores escalados del índice 1543 al 2002
-    test_data = scaled_data[training_data_len - 60: , :]
-    # Create the data sets x_test and y_test
-    x_test = []
-    y_test = dataset[training_data_len:, :]
-    for i in range(60, len(test_data)):
-        x_test.append(test_data[i-60:i, 0])
-            
-    # Convert the data to a numpy array
-    # Convierte los datos en una matriz numpy
-    x_test = np.array(x_test)
-
-    # Reshape the data
-    # Reforma los datos
-    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1 ))
-
-    # Get the models predicted price values 
-    # Obtenga los valores de precios predichos de los modelos
-    #convertir x_test en un valor de tensor
-    x_test = tf.convert_to_tensor(x_test)
-    predictions = model.predict(x_test)
-    predictions = scaler.inverse_transform(predictions)
-
-    # Get the root mean squared error (RMSE)
-    # Obtener la raíz del error cuadrático medio (RMSE)
-    rmse = np.sqrt(np.mean(((predictions - y_test) ** 2)))
-    rmse
-
-    # Graficos Finales
-    train = data[:training_data_len]
-    valid = data[training_data_len:]
-    valid['Predictions'] = predictions
-    # Visualizando la data
-    st.subheader('Comparacion de entrenamiento y validacion ')
-    fig2=plt.figure(figsize=(16,6))
-    plt.title('Model')
-    plt.xlabel('Tiempo', fontsize=18)
-    plt.ylabel('Precio dolar ($)', fontsize=18)
-    plt.plot(train['Close'])
-    plt.plot(valid[['Predictions']])
-    plt.legend(['Train', 'Val', 'Predictions'], loc='lower right')
-    plt.show()
-    st.pyplot(fig2)
-    
-    # Visualizando la data 2
-    st.subheader('Comparacion de entrenamiento, validacion y prediccion')
-    fig3=plt.figure(figsize=(16,6))
-    plt.title('Model')
-    plt.xlabel('Tiempo', fontsize=18)
-    plt.ylabel('Precio dolar ($)', fontsize=18)
-    plt.plot(train['Close'])
-    plt.plot(valid[['Close','Predictions']])
-    plt.legend(['Train', 'Val', 'Predictions'], loc='lower right')
-    plt.show()
-    st.pyplot(fig3)
-
-    # Visualizando la data 3
-    st.subheader('Precio predecido vs Precio Original')
-    fig4=plt.figure(figsize=(12,6))
-    plt.plot(y_test, 'b', label = 'Precio Original')
-    plt.plot(predictions, 'r', label= 'Precio Predecido')
-    plt.xlabel('Tiempo')
-    plt.ylabel('Precio')
-    plt.legend()
-    st.pyplot(fig4)
-    
-    # Visualizando datos
-    st.subheader('Mostrar los datos originales y predecidos') 
-    st.write(valid)
+    # remodelarlo [muestras, pasos de tiempo, características]
+    X_train = np.reshape(X_train, (X_train.shape[0], 36, 1))
 
 
 
-##########PLANTILLA###################
-    # Evaluación del modelo
-    
-    st.title('Evaluación del Modelo LSTM')
-    ## Métricas
-    MAE=metrics.mean_absolute_error(y_test, predictions)
-    MSE=metrics.mean_squared_error(y_test, predictions)
-    RMSE=np.sqrt(metrics.mean_squared_error(y_test, predictions))
-    
-    metricas = {
-        'metrica' : ['Mean Absolute Error', 'Mean Squared Error', 'Root Mean Squared Error'],
-        'valor': [MAE, MSE, RMSE]
-    }
-    metricas = pd.DataFrame(metricas)  
-    ### Gráfica de las métricas
-    st.subheader('Métricas de rendimiento') 
-    fig = px.bar(        
-        metricas,
-        x = "metrica",
-        y = "valor",
-        title = "Métricas del Modelo LSTM",
-        color="metrica"
-    )
+    import keras
+    from keras.layers import Dense
+    from keras.layers import LSTM
+    from keras.layers import Dropout
+
+    # Construye el modelo
+    model = keras.Sequential()
+
+    model.add(LSTM(units = 100, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+    model.add(Dropout(0.2))
+
+    model.add(LSTM(units = 100))
+    model.add(Dropout(0.2))
+
+    # Capa de salida
+    model.add(Dense(units = 1))
+
+    # Compilando el modelo
+    model.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+    # Ajuste del modelo al conjunto de entrenamiento
+    history = model.fit(X_train, y_train, epochs = 20, batch_size = 10, validation_split=.30)
+
+    # Plot training & validation loss values with streamlit
+    st.subheader('Gráfico de pérdida de entrenamiento y validación')
+    fig = px.line(history.history, y=['loss', 'val_loss'], title='Pérdida de entrenamiento y validación')
     st.plotly_chart(fig)
+
+
+    # Obtenga los precios de las acciones para 2019 para que nuestro modelo haga las predicciones
+    test_data = test_df['Adj Close'].values
+    test_data = test_data.reshape(-1,1)
+    test_data = scaler.transform(test_data)
+
+    # Cree los datos para probar nuestro modelo en:
+    time_steps = 36
+    X_test, y_test = create_dataset(test_data, time_steps)
+
+    # almacenar los valores originales para trazar las predicciones
+    y_test = y_test.reshape(-1,1)
+    org_y = scaler.inverse_transform(y_test)
+
+    # remodelarlo [muestras, pasos de tiempo, características]
+    X_test = np.reshape(X_test, (X_test.shape[0], 36, 1))
+
+    # Predecir los precios con el modelo.
+    predicted_y = model.predict(X_test)
+    predicted_y = scaler.inverse_transform(predicted_y)
+
+    # Graficar los resultados con streamlit
+    st.subheader('Gráfico de predicción de precios de cierre de '+user_input)
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=org_dates, y=org_y.reshape(-1), name='Real'))
+    fig.add_trace(go.Scatter(x=org_dates[36:], y=predicted_y.reshape(-1), name='Predicción'))
+    st.plotly_chart(fig)
+
+    #mostrar en una tabla los valores reales y los predichos
+    st.subheader('Tabla de valores reales y predichos')
+    df = pd.DataFrame({'Real': org_y.reshape(-1), 'Predicho': predicted_y.reshape(-1)})
+    st.write(df)
+
+    #imprimir la evaluación del modelo
+    from sklearn.metrics import mean_squared_error
+    from sklearn.metrics import mean_absolute_error
+    from sklearn.metrics import r2_score
+
+    st.subheader('Evaluación del modelo')
+    st.write('MSE: '+str(mean_squared_error(org_y, predicted_y)))
+    st.write('MAE: '+str(mean_absolute_error(org_y, predicted_y)))
+    st.write('R2: '+str(r2_score(org_y, predicted_y)))
+
+    # Graficar en barras las metricas de evaluación con streamlit
+    st.subheader('Gráfico de métricas de evaluación')
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=['MSE', 'MAE', 'R2'], y=[mean_squared_error(org_y, predicted_y), mean_absolute_error(org_y, predicted_y), r2_score(org_y, predicted_y)], name='Metricas'))
+    st.plotly_chart(fig)
+   
