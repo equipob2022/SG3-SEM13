8c9b50a56bfe1e11bb52a4021ab32ffe79073d8c
Kevin Ramos Rivas <80496624+KevinRamosRivas@users.noreply.github.com>
2022-12-10 00:45:18 -0500

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,15 +11,11 @@ st.markdown("""
 """)
 # Add all your application here
 app.add_app("Home", home.app)
-# app.add_app("Modelo SVR", model1.app)
-# app.add_app("PCA and Hierarchical Portfolio Optimisation", model12.app)
-app.add_app("Modelo LSTM", model9.app)
 app.add_app("Modelo XGBoost", model10.app)
+app.add_app("Modelo LSTM", model9.app)
 app.add_app("Modelo Random Forest Regressor", model11.app)
 app.add_app("PCA and Hierarchical Portfolio Optimisation", model12.app)
 
 
 # The main app
-
-
 app.run()
